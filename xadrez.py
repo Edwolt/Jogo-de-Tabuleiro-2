368c8de63e7d88c046733b1d4928d21e9ef12f08
Edws <eduardos.rocha17@gmail.com>
2020-09-08 12:31:21 -0300

    Usando type notation e docstring

--- xadrez.py
+++ xadrez.py
@@ -1,8 +1,18 @@
-class Xadrez:
-    
+import pygame
 
-    def event(self, event):
+
+class Xadrez:
+    def event(self, event: pygame.event) -> None:
+        """
+        Recebe um evento e executa uma operação com ele
+
+        :param event: evento
+        """
         pass
 
-    def draw(self, screen):
+    def draw(self, screen: pygame.surface) -> bool:
+        """[summary]
+        :param screen: Surface onde sera desenhado o jogo sera desenhado
+        :return: Retorna se houve mudança ou nao na tela
+        """
         return False
