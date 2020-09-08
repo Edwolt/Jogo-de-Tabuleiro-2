8151cba6d4b30fa564d55ef9d84d6b649dda6745
Edws <eduardos.rocha17@gmail.com>
2020-09-08 15:19:39 -0300

    Mudando nome de variaveis
    Não tem porquê deixar as variáveis privadas por enquanto

--- src/xadrez.py
+++ src/xadrez.py
@@ -6,15 +6,20 @@ class Xadrez:
     """Toda a lógica do jogo"""
 
     def __init__(self):
-        self.__atualizacao: bool = True
-        self.__tabuleiro: list = tabuleiro_none()
+        self.atualizacao: bool = True
+        self.tabuleiro: list = tabuleiro_none()
         self.cores: tuple = (
             (214, 165, 132),
             (124, 49, 0)
         )
         self.click_color: tuple = (153, 0, 0)
-        self.__click = None
-        self.__qsize: tuple = (0, 0)
+        self.click = None
+        self.qsize: tuple = (0, 0)
+        self.pecas = Pecas()
+
+    def carregar(self):
+        self.pecas.carregar()
+        self.tabuleiro = tabuleiro_none
 
     def event(self, event: pygame.event) -> None:
         """
@@ -24,11 +29,11 @@ class Xadrez:
         """
 
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-            self.__click = (
-                event.pos[0] // self.__qsize[0],
-                event.pos[1] // self.__qsize[1]
+            self.click = (
+                event.pos[0] // self.qsize[0],
+                event.pos[1] // self.qsize[1]
             )
-            self.__atualizacao = True
+            self.atualizacao = True
 
     def draw(self, screen: pygame.Surface) -> bool:
         """
@@ -38,10 +43,10 @@ class Xadrez:
 
         size = screen.get_size()
         qsize = (size[0] / 8, size[1] / 8)
-        self.__qsize = qsize
+        self.qsize = qsize
 
-        if self.__atualizacao:
-            for x, linha in enumerate(self.__tabuleiro):
+        if self.atualizacao:
+            for x, linha in enumerate(self.tabuleiro):
                 for y, peca in enumerate(linha):
                     surf = pygame.Surface(qsize)
                     color = self.cores[(x + y) % 2]
@@ -49,13 +54,13 @@ class Xadrez:
                     pos = (qsize[0] * x, qsize[1] * y)
                     screen.blit(surf, pos)
 
-            if self.__click:
+            if self.click:
                 surf = pygame.Surface(qsize)
                 surf.fill(self.click_color)
                 screen.blit(
-                    surf, (self.__click[0] * qsize[0], self.__click[1] * qsize[1]))
+                    surf, (self.click[0] * qsize[0], self.click[1] * qsize[1]))
 
-            self.__atualizacao = False
+            self.atualizacao = False
             return True
         else:
             return False
