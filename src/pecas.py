7a931d1adc42ab8f94272fa937979d8b20449979
Edws <eduardos.rocha17@gmail.com>
2020-09-27 19:56:01 -0300

    Transformando pecas.py e movimentos.py em um único arquivo
    Separar essas duas classes em arquivos diferentes não aumenta a legibilidade do código

--- src/pecas.py
+++ src/pecas.py
@@ -16,7 +16,30 @@ Ideia para fazer o movimento:
 """
 
 
-##### Peças #####
+##### Classes Abstratas #####
+def mover_peca(tabuleiro: list, pos: tuple, nova_pos: tuple) -> None:
+    i, j = pos
+    m, n = nova_pos
+    tabuleiro[m][n] = tabuleiro[i][j]
+    tabuleiro[m][n].notifica_movimento()
+    tabuleiro[i][j] = None
+
+
+class MovimentoEspecial():
+    """Classe abstrata para os movimentos especiais"""
+
+    def executar(self, tabuleiro: list, criador_pecas, flags: list) -> None:
+        """
+        Executa o movimento no tabuleiro
+        :param flags: lista de flags do tabuleiro
+        """
+        pass
+
+    def update_flags(self, flags: list) -> None:
+        """Atualiza a lista de flags do tabuleiro"""
+        pass
+
+
 def valida_coordenadas(a: int, b: int = 0) -> bool:
     """
     valida_coordenada(a):    Verifica se a é um valor válido para componente de uma coordenada
@@ -63,6 +86,27 @@ class Peca():
         Caso o movimento seja especial é retornado um objeto de uma subclasse de M
         """
         pass
+
+
+##### Rei #####
+class Roque(MovimentoEspecial):
+    def __init__(self, rei: tuple, nova_rei: tuple, torre: tuple, nova_torre):
+        """[summary]
+        :param rei: posição atual do rei
+        :param nova_rei: posiçãol para a qual o rei será movido
+        :param torre: posição atual da torre
+        :param nova_torre: posição para o qual a torre será movida
+        """
+
+        self.nome = 'roque'
+        self.rei = rei
+        self.nova_rei = nova_rei
+        self.torre = torre
+        self.nova_torre = nova_torre
+
+    def executar(self, tabuleiro: list, flags: list, criador_pecas) -> None:
+        mover_peca(tabuleiro, self.rei, self.nova_rei)
+        mover_peca(tabuleiro, self.torre, self.nova_torre)
 
 
 class Rei(Peca):
@@ -132,6 +176,7 @@ class Rei(Peca):
         return res
 
 
+##### Rainha #####
 class Rainha(Peca):
     def __init__(self, sprite: Surface, cor: bool):
         self.nome = 'rainha'
@@ -154,6 +199,7 @@ class Rainha(Peca):
         return res
 
 
+##### Bispo #####
 class Bispo(Peca):
     def __init__(self, sprite: Surface, cor: bool):
         self.nome = 'bispo'
@@ -172,6 +218,7 @@ class Bispo(Peca):
         return res
 
 
+##### Cavalo #####
 class Cavalo(Peca):
     def __init__(self, sprite: Surface, cor: bool):
         self.nome = 'cavalo'
@@ -213,6 +260,7 @@ class Cavalo(Peca):
         return res
 
 
+##### Torre #####
 class Torre(Peca):
     def __init__(self, sprite: Surface, cor: bool, movimentou: bool = False):
         self.nome = 'torre'
@@ -234,6 +282,75 @@ class Torre(Peca):
         )
         calcula_direcao(res, tabuleiro, pos, direcoes, self.cor)
         return res
+
+
+##### Peão #####
+class Promocao(MovimentoEspecial):
+    def __init__(self, pos: tuple, promocao: tuple):
+        """
+        :param pos: posição atual do peão
+        :param promocao: posição para o qual o peão será movido causando a promoção
+        """
+
+        self.nome = 'promocao'
+        self.pos = pos
+        self.promocao = promocao
+        pass
+
+    def executar(self, tabuleiro: list, flags: list, criador_pecas) -> None:
+        i, j = self.pos
+        cor = tabuleiro[i][j].cor
+        tabuleiro[i][j] = None
+
+        i, j = self.promocao
+        tabuleiro[i][j] = criador_pecas.Rainha(cor)
+
+
+class AvancoDuplo(MovimentoEspecial):
+    def __init__(self, cor: bool, pos: tuple, meio: tuple, nova_pos: tuple):
+        """
+        :param cor: cor da peça (True: 'branco'; False: 'preto')
+        :param pos: posicão do peão
+        :param meio: posição pela qual o peão passará
+        :param nova_pos: posição final do peão
+        """
+
+        self.cor = cor
+        self.pos = pos
+        self.meio = meio
+        self.nova_pos = nova_pos
+
+    def executar(self, tabuleiro: list, flags: list, criador_pecas) -> None:
+        mover_peca(tabuleiro, self.pos, self.nova_pos)
+
+    def update_flags(self, flags: list) -> None:
+        flags.append(
+            (
+                'enpassant',
+                self.cor,
+                self.meio,
+                self.nova_pos
+            )
+        )
+
+
+class EnPassant(MovimentoEspecial):
+    def __init__(self, pos: tuple, capturado_pos: tuple, nova_pos: tuple):
+        """
+        :param pos: posição do peão aliado
+        :param capturado_pos: posição do peão inimigo a ser capturado
+        :param nova_pos: posição para o qual o peão aliado será movido
+        """
+
+        self.nome = 'enpassant'
+        self.pos = pos
+        self.capturado_pos = capturado_pos
+        self.nova_pos = nova_pos
+
+    def executar(self, tabuleiro: list, flags: list, criador_pecas) -> None:
+        mover_peca(tabuleiro, self.pos, self.nova_pos)
+        i, j = self.capturado_pos
+        tabuleiro[i][j] = None
 
 
 class Peao(Peca):
@@ -298,7 +415,7 @@ class Peao(Peca):
         return res
 
 
-##### Exportando Peças #####
+##### Criador de Peças #####
 # TODO id e identificador não são bons nomes de variáveis
 def id_peca(nome: str, cor: bool) -> str:
     """Retorna o identificador do tipo de peca"""
