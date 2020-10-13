61b4c4a70f159382c00408904e65471a6d139ae4
Edws <eduardos.rocha17@gmail.com>
2020-10-13 20:51:40 -0300

    Agora o sistema de menus aceita submenus

--- src/menu.py
+++ src/menu.py
@@ -10,33 +10,87 @@ from glob import glob
 from recursos import Recursos
 
 
-class Opcao:
-    def __init__(self, nome, funcao):
-        self.nome = nome
-        self.funcao = funcao
+class Submenu:
+    def __init__(self):
+        self.sel = 0
+
+    def tamanho(self) -> int:
+        return len(self.opcoes)
+
+    def nome(self, i=None) -> str:
+        return self.opcoes[self.sel if i is None else i]
+
+    def listar(self) -> tuple:
+        for i in range(self.tamanho()):
+            yield self.sel == i, self.nome(i)
+
+    def executar(self, i=None):
+        self.funcoes[self.sel if i is None else i]()
+
+    def voltar(self):
+        return self.anterior
 
 
-def sair():
-    pygame.quit()
-    quit(0)
+class MenuConfigs(Submenu):
+    def __init__(self, anterior):
+        super().__init__()
+        self.anterior = anterior
+        self.configs = self.listar_configs()
+
+    def listar_configs(self) -> list:
+        # 8 por causa do nome da pasta
+        # -3 por causa da extensao
+        return [i[8:-3] for i in glob('configs/*py')]
+
+    ##### Interface #####
+    def tamanho(self) -> int:
+        return len(self.configs) + 1
+
+    def nome(self, i=None) -> str:
+        i = self.sel if i is None else i
+        if 0 <= i < len(self.configs):
+            return self.configs
+        else:
+            return 'Voltar'
+
+    def executar(self, i=None):
+        i = self.sel if i is None else i
+        self.recursos.set_config(self.configs[self.sel])
 
 
-primeiro_menu = (
-    Opcao('Config', lambda: print('Config não implementado')),
-    Opcao('Imagens', lambda: print('Imagens não implementado')),
-    Opcao('Fonte', lambda: print('Fonte não implementado')),
-    Opcao('Desfazer', lambda: print('Desfazer não implementado')),
-    Opcao('Voltar', lambda: print('Voltar não implementado')),
-    Opcao('Sair', sair)
-)
+class MenuPrincipal(Submenu):
+    def __init__(self):
+        super().__init__()
+        self.opcoes = (
+            'Config',
+            'Imagens',
+            'Fonte',
+            'Desfazer',
+            'Voltar',
+            'Sair'
+        )
+
+    ##### Interface #####
+    def executar(self, i=None):
+        i = self.sel if i is None else i
+        opcao = self.nome(i).lower()
+        if opcao == 'config':
+            return MenuConfigs(self)
+        elif opcao == 'sair':
+            pygame.quit()
+            quit(0)
+        else:
+            print(f'{opcao} não implementado')
+
+    def voltar(self):
+        return None
 
 
 class Menu:
-    def __init__(self, xadrez, recursos: Recursos, opcoes: tuple = primeiro_menu):
+    def __init__(self, xadrez, recursos: Recursos, opcoes: Submenu = MenuPrincipal()):
         self.xadrez = xadrez
         self.recursos = recursos
 
-        self.escape = False
         self.enter = None
         self.atualizacao = True
 
@@ -46,36 +100,29 @@ class Menu:
         )
         self.opcoes = opcoes
 
-        self.sel = 0
-
-    def listar_configs(self) -> list:
-        # 8 por causa do nome da pasta
-        # -3 por causa da extensao
-        return [i[8:-3] for i in glob('configs/*py')]
-
     ##### Interface #####
     def event(self, event: Event) -> None:
         if event.type == KEYDOWN:
             if event.key == K_UP:
                 self.atualizacao = True
-                if self.sel > 0:
-                    self.sel -= 1
+                if self.opcoes.sel > 0:
+                    self.opcoes.sel -= 1
                 else:
-                    self.sel = len(self.opcoes) - 1
+                    self.sel = self.opcoes.tamanho() - 1
 
             elif event.key == K_DOWN:
                 self.atualizacao = True
-                if self.sel < len(self.opcoes) - 1:
-                    self.sel += 1
+                if self.opcoes.sel < self.opcoes.tamanho() - 1:
+                    self.opcoes.sel += 1
                 else:
-                    self.sel = 0
+                    self.opcoes.sel = 0
 
             elif event.key == K_RETURN:
                 self.atualizacao = True
-                self.opcoes[self.sel].funcao()
+                self.opcoes.executar()
 
             elif event.key == K_ESCAPE:
-                self.escape = True
+                self.opcoes = self.opcoes.voltar()
 
     def draw(self, canva: Surface) -> None:
         if not self.atualizacao:
@@ -85,14 +132,12 @@ class Menu:
 
         altura = self.fonte.size('')[1]
         y = 0
-        for num, i in enumerate(self.opcoes):
-            texto_str = '> ' if self.sel == num else '  '
-            texto_str += i.nome
-
+        for selecionado, nome in self.opcoes.listar():
+            texto_str = ('> ' if selecionado else '  ') + nome
             texto = self.fonte.render(
                 texto_str,
                 0,
-                self.recursos.config.menu_cor(texto_str, self.sel == num)
+                self.recursos.config.menu_cor(texto_str, selecionado)
             )
             canva.blit(texto, (0, y))
             y += altura
@@ -102,10 +147,7 @@ class Menu:
         display.flip()
 
     def new(self):
-        if self.enter:
-            return self.enter
-        elif self.escape:
-            self.escape = False
+        if self.opcoes is None:
             return self.xadrez
         else:
             return self
