e248a6d86a31dc2df3126e5003bd4e2f4fc8f244
Edws <eduardos.rocha17@gmail.com>
2020-10-19 14:03:46 -0300

    Fazendo a cor das peças ser calculada durante o loading

--- src/recursos.py
+++ src/recursos.py
@@ -1,5 +1,5 @@
 from pygame import image
-from pygame import Surface
+from pygame import Color, Surface
 
 import importlib
 
@@ -17,7 +17,7 @@ def id_peca(nome: str, cor: bool) -> str:
 def todos_ids() -> list:
     """Retorna uma lista com todos os identificadores de peças"""
     nome_pecas = ['rei', 'rainha', 'bispo', 'cavalo', 'torre', 'peao']
-    return [id_peca(i, False) for i in nome_pecas] + [id_peca(i, True) for i in nome_pecas]
+    return [(i, id_peca(i, False)) for i in nome_pecas] + [(i, id_peca(i, True)) for i in nome_pecas]
 
 
 def caminho_asset(identificador: str) -> str:
@@ -36,14 +36,36 @@ class Recursos:
     def set_config(self, config: str) -> None:
         self.config = Config(config)
 
+    def gerar_imagem(self, sprite: Surface, grad: tuple) -> Surface:
+        res = sprite.copy()
+        cor1, cor2 = grad
+
+        w, h = res.get_size()
+
+        for i in range(w):
+            for j in range(h):
+                cor_atual = res.get_at((i, j))
+                nova_cor = Color(
+                    cor1.r + (cor1.r + cor2.r) * (cor_atual.r // 255),
+                    cor1.g + (cor1.g + cor2.g) * (cor_atual.g // 255),
+                    cor1.b + (cor1.b + cor2.b) * (cor_atual.b // 255),
+                    cor_atual.a
+                )
+                res.set_at((i, j), nova_cor)
+
+        return res
+
     def carregar(self):
         """Carrega os assets das peças em RAM"""
         identificadores = todos_ids()
         n = len(identificadores)
 
         yield [(n, 0)]
-        for k, i in enumerate(identificadores):
-            self.assets[i] = image.load(caminho_asset(i))
+        for k, (nome, i) in enumerate(identificadores):
+            self.assets[i] = self.gerar_imagem(
+                image.load(caminho_asset(nome)),
+                (Color(0, 0, 0), Color(100, 100, 100))
+            )
             yield [(n, k)]
         yield [(n, n)]
 
