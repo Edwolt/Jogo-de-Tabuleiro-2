af2a3767e43378bd4f681c734c9f481e9a961fe1
Edws <eduardos.rocha17@gmail.com>
2020-10-19 15:18:46 -0300

    Refatorando a parte de assets de recursos

--- src/recursos.py
+++ src/recursos.py
@@ -8,21 +8,9 @@ def Config(nome: str):
     return importlib.import_module(f'configs.{nome}').Config()
 
 
-# TODO id e identificador não são bons nomes de variáveis
-def id_peca(nome: str, cor: bool) -> str:
-    """Retorna o identificador do tipo de peça"""
-    return nome + ('1' if cor else '0')
-
-
-def todos_ids() -> list:
-    """Retorna uma lista com todos os identificadores de peças"""
-    nome_pecas = ['rei', 'rainha', 'bispo', 'cavalo', 'torre', 'peao']
-    return [(i, id_peca(i, False)) for i in nome_pecas] + [(i, id_peca(i, True)) for i in nome_pecas]
-
-
-def caminho_asset(identificador: str) -> str:
+def caminho_asset(nome: str) -> str:
     """Retorna o caminho para o asset da peca com o identificador passado"""
-    return f'assets/{identificador}.png'
+    return f'assets/{nome}.png'
 
 
 class Recursos:
@@ -36,40 +24,42 @@ class Recursos:
     def set_config(self, config: str) -> None:
         self.config = Config(config)
 
-    def gerar_imagem(self, sprite: Surface, grad: tuple) -> Surface:
-        res = sprite.copy()
-        cor1, cor2 = grad
+    def gerar_imagem(self, sprite: Surface, grad_preto: tuple, grad_branco: tuple) -> tuple:
+        preto = sprite.copy()
+        p1, p2 = grad_preto
+        branco = sprite.copy()
+        b1, b2 = grad_branco
 
-        w, h = res.get_size()
-
-        from random import randint
+        w, h = sprite.get_size()
 
         for i in range(w):
             for j in range(h):
-                cor_atual = res.get_at((i, j))
-                nova_cor = Color(0, 0, 0, 0)
+                atual = sprite.get_at((i, j))
+                np = Color(0, 0, 0, 0)
+                nb = Color(0, 0, 0, 0)
 
-                for k in range(len(nova_cor)):
-                    novo = cor1[k] + (cor2[k] - cor1[k]) * (cor_atual[k] / 255)
-                    nova_cor[k] = int(novo)
+                for k in range(len(atual)):
+                    np[k] = int(p1[k] + (p2[k] - p1[k]) * (atual[k] / 255))
+                    nb[k] = int(b1[k] + (b2[k] - b1[k]) * (atual[k] / 255))
 
-                res.set_at((i, j), nova_cor)
+                preto.set_at((i, j), np)
+                branco.set_at((i, j), nb)
 
-        return res
+        return preto, branco
 
     def carregar(self):
-        """Carrega os assets das peças em RAM"""
-        identificadores = todos_ids()
-        n = len(identificadores)
-
+        """Carrega os assets das peças na memória"""
+        nome_pecas = ['rei', 'rainha', 'bispo', 'cavalo', 'torre', 'peao']
+        n = len(nome_pecas)
         yield [(n, 0)]
-        for k, (nome, i) in enumerate(identificadores):
+        for k, i in enumerate(nome_pecas):
             self.assets[i] = self.gerar_imagem(
-                image.load(caminho_asset(nome)),
-                (Color(0, 0, 0, 0), Color(100, 0, 0, 255))
+                image.load(caminho_asset(i)),
+                (Color(0, 0, 0, 0), Color(100, 100, 100, 255)),
+                (Color(100, 100, 100, 0), Color(255, 255, 255, 255))
             )
             yield [(n, k+1)]
 
     def get_asset(self, nome: str, cor: bool) -> Surface:
         """Retorna o asset da peca com o nome e a cor dada"""
-        return self.assets[id_peca(nome, cor)]
+        return self.assets[nome][1 if cor else 0]
