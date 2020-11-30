2c50cc0da255cfaa8fe74439cc0658316a26b76e
Edws <eduardos.rocha17@gmail.com>
2020-11-30 17:52:52 -0300

    Colocando o texto Loading na tela de Loading

--- src/loading.py
+++ src/loading.py
@@ -11,6 +11,7 @@ class Loading():
         self.pronto = False
         self.carregamento = carregamento
         self.fonte = recursos.config.fonte(25)
+        self.fonte_loading = recursos.config.fonte(50)
 
     def event(self, event: Event) -> None:
         pass
@@ -21,17 +22,25 @@ class Loading():
         cor_falta = Color(255, 0, 0)
         cor_carregado = Color(0, 255, 0)
 
+        try:
+            barras = next(self.carregamento)
+        except StopIteration:
+            self.pronto = True
+            return
+
         x = 10
         y = 10
         espaco = 10
         h = self.fonte.size('')[1]
         w = canvas.get_width() - 2 * x
 
-        try:
-            barras = next(self.carregamento)
-        except StopIteration:
-            self.pronto = True
-            return
+        texto = 'Loading'
+        xx, yy = self.fonte_loading.size(texto)
+        meio = canvas.get_width()/2 - xx/2
+        texto = self.fonte_loading.render(texto, 0, Color(255, 255, 255))
+        canvas.blit(texto, (meio, y))
+
+        y += yy + 2 * espaco
 
         for tam, val in barras:
             draw.rect(
@@ -47,10 +56,9 @@ class Loading():
 
             texto = f'{val} / {tam}'
             xx, yy = self.fonte.size(texto)
-            meio = canvas.get_width() / 2 - xx
+            meio = canvas.get_width()/2 - xx/2
             texto = self.fonte.render(texto, 0, Color(255, 255, 255))
             canvas.blit(texto, (meio, y))
-
 
             y += yy + espaco
 
