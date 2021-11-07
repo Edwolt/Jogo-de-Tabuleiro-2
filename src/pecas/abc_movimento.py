c66f8b2cd54c9929d154620d6b33320e0d995cd0
Eduardo Souza Rocha <eduardos.rocha17@gmail.com>
2021-11-06 22:01:08 -0300

    Tornando Movimento uma classe concreta
    Tornando Movimento uma classe concreta e colocando código do executar

--- src/pecas/abc_movimento.py
+++ src/pecas/abc_movimento.py
@@ -3,18 +3,23 @@ from abc import ABC, abstractmethod
 from tipos import matriz_tabuleiro, mov
 
 
-class Movimento(ABC):
+class Movimento:
     """Classe abstrata para os movimentos especiais"""
 
-    def __init__(self, *, nome: str):
-        self.nome = nome
+    def __init__(self, movimento: mov, rei: bool = False):
+        self.movimento = movimento
+        self.rei = rei
 
-    @abstractmethod
     def executar(self, tabuleiro: matriz_tabuleiro, flags: list) -> None:
         """
         Executa o movimento no tabuleiro
         :param flags: lista de flags do tabuleiro
         """
+        (i, j), (m, n) = self.movimento
+
+        tabuleiro[m][n] = tabuleiro[i][j]
+        tabuleiro[i][j] = None
+        tabuleiro[m][n].notifica_movimento()
 
     def update_flags(self, flags: list) -> None:
         """Atualiza a lista de flags do tabuleiro"""
